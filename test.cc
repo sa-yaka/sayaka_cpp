9d4c24583f041f9f15c02a31a8a791d829fad73c
sa-yaka <saya.ka@zohomail.jp>
2022-04-10 18:25:18 +0800

    test

--- test.cc
+++ test.cc
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <stdio.h>
+#include <wiringPi.h>
 
-using namespace std;
 int main() {
-  //...
+  wiringPiSetup();
+  // int i = 0;
+  pinMode(1, OUTPUT);
+  while (1) {
+    printf("turn off led\n");
+    digitalWrite(1, HIGH);
+    delay(1000);
+    printf("turn on led\n");
+    digitalWrite(1, LOW);
+    delay(1000);
+  }
   return 0;
 }
