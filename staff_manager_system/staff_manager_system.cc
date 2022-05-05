8572d28fb618d0adeba28fc051fec19e3844fbd9
sa-yaka <sx.sayaka@gmail.com>
2022-05-05 21:12:53 +0800

    Staff manager

--- staff_manager_system/staff_manager_system.cc
+++ staff_manager_system/staff_manager_system.cc
@@ -24,6 +24,11 @@ struct Addressbooks {
 void ShowMenu();
 void AddStaff(Addressbooks *staff);
 void ShowStaff(Addressbooks *staff);
+int IsExist(Addressbooks *staff, string name);
+void RemoveStaff(Addressbooks *staff);
+void FindStaff(Addressbooks *staff);
+void ResetStaff(Addressbooks *staff);
+void CleanStaff(Addressbooks *staff);
 
 int main() {
   Addressbooks staff;
@@ -40,6 +45,22 @@ int main() {
         break;
       case 2:
         ShowStaff(&staff);
+        break;
+      case 3:
+        RemoveStaff(&staff);
+        break;
+      case 4:
+        FindStaff(&staff);
+        break;
+      case 5:
+        ResetStaff(&staff);
+        break;
+      case 6:
+        CleanStaff(&staff);
+        break;
+      case 0:
+        cout << "退出" << endl;
+        return 0;
         break;
     }
   }
@@ -73,7 +94,8 @@ void AddStaff(Addressbooks *staff) {
     string address;
 
     cout << "输入姓名\n:) ";
-    cin >> name;
+    cin.get();
+    getline(cin, name);
     staff->StaffArray[staff->many_staff].name = name;
 
     cout << "输入性别：\n0 -- 男\n1 -- 女\n"
@@ -98,7 +120,8 @@ void AddStaff(Addressbooks *staff) {
     staff->StaffArray[staff->many_staff].phone = phone;
 
     cout << "输入地址\n:) ";
-    cin >> address;
+    cin.get();
+    getline(cin, address);
     staff->StaffArray[staff->many_staff].address = address;
     staff->many_staff++;
 
@@ -111,7 +134,7 @@ void ShowStaff(Addressbooks *staff) {
   if (staff->many_staff == 0) {
     cout << "当前记录为空\n:) ";
   } else {
-    for (int i = 0; i < staff->many_staff; i++) {
+    for (int i = 0; i <= staff->many_staff; i++) {
       cout << "姓名：" << staff->StaffArray[i].name << endl;
       cout << "性别：" << (staff->StaffArray[i].sex == 0 ? "男" : "女") << endl;
       cout << "年龄：" << staff->StaffArray[i].age << endl;
@@ -121,4 +144,107 @@ void ShowStaff(Addressbooks *staff) {
       ShowMenu();
     }
   }
+}
+
+int IsExist(Addressbooks *staff, string name) {
+  for (int i = 0; i <= staff->many_staff; i++) {
+    if (staff->StaffArray[i].name == name) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void RemoveStaff(Addressbooks *staff) {
+  string name;
+  cout << "输入要删除的职员\n:) ";
+  cin.get();
+  getline(cin, name);
+
+  int check = IsExist(staff, name);
+  if (check == -1) {
+    cout << "没有相关记录" << endl;
+  } else {
+    for (int i = 0; i <= staff->many_staff; i++) {
+      staff->StaffArray[i] = staff->StaffArray[i + 1];
+    }
+    staff->many_staff--;
+    cout << "删除成功" << endl;
+  }
+}
+
+void FindStaff(Addressbooks *staff) {
+  cout << "输入要删除的职员\n:)";
+  string name;
+  cin.get();
+  getline(cin, name);
+
+  int check = IsExist(staff, name);
+  if (check == -1) {
+    cout << "没有相关记录" << endl;
+  } else {
+    cout << "姓名：" << staff->StaffArray[check].name << endl;
+    cout << "性别：" << (staff->StaffArray[check].sex == 0 ? "男" : "女")
+         << endl;
+    cout << "年龄：" << staff->StaffArray[check].age << endl;
+    cout << "电话：" << staff->StaffArray[check].phone << endl;
+    cout << "地址：" << staff->StaffArray[check].address << endl;
+    cout << "Done\n" << endl;
+  }
+}
+
+void ResetStaff(Addressbooks *staff) {
+  cout << "输入要重设的职员姓名\n:) ";
+  string name;
+  cin.get();
+  getline(cin, name);
+
+  int check = IsExist(staff, name);
+
+  if (check == -1) {
+    cout << "没有相关记录";
+  } else {
+    int sex;
+    int age;
+    // double salaru;
+    string phone;
+    string address;
+
+    staff->StaffArray[staff->many_staff].name = name;
+
+    cout << "输入性别：\n0 -- 男\n1 -- 女\n"
+         << ":) ";
+    while (true) {
+      cin >> sex;
+      if (sex == 0 || sex == 1) {
+        staff->StaffArray[staff->many_staff].sex = sex;
+        break;
+      } else {
+        cout << "输入有误，请重新输入\n:) ";
+        continue;
+      }
+    }
+
+    cout << "输入年龄\n:) ";
+    cin >> age;
+    staff->StaffArray[staff->many_staff].age = age;
+
+    cout << "输入电话号码\n:) ";
+    cin >> phone;
+    staff->StaffArray[staff->many_staff].phone = phone;
+
+    cout << "输入地址\n:) ";
+    cin.get();
+    getline(cin, address);
+    staff->StaffArray[staff->many_staff].address = address;
+    staff->many_staff++;
+
+    cout << "修改成功\n" << endl;
+    ShowMenu();
+  }
+}
+
+void CleanStaff(Addressbooks *staff) {
+  staff->many_staff = 0;
+  cout << "记录已清空" << endl;
 }
